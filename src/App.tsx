479fde23afb1bc32fa2701ca05a7f10610576596
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-12-10 20:25:08 -0500

    ":poop: unknown"

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2 className='text-center text-4xl'>Alexi Dg</h2>
+      <h2 className='text-center text-4xl font-bold'>Alexi Dg</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
