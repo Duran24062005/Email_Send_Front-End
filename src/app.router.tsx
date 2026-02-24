2ba71a70a95cd20b4d61a75fe3c4b4c4e2ac1992
Alexi Dg <alexisdurangomez588@gmail.com>
2026-02-23 19:33:21 -0500

    feat: :sparkles: the dev enviroments has been finished and reat routers has been added

---
+++ src/app.router.tsx
@@ -0,0 +1,35 @@
+import { lazy } from 'react';
+import { createBrowserRouter, Navigate } from 'react-router';
+import { LoginPage } from './auth/pages/login/LoginPage';
+import { RegisterPage } from './auth/pages/register/RegisterPage';
+import HomeLayout from './home/layouts/HomeLayout';
+
+const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
+
+
+export const appRouter = createBrowserRouter([
+    // Main routes
+  {
+    path: '/',
+    element: <HomeLayout />
+  },
+    // Auth Routes
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/auth/login" />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
